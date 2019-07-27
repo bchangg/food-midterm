d2bd2418a4f3e6f5dc3fd39914b373cde7643a36
Brian Chang <changbhc@gmail.com>
2019-07-27 16:51:46 +0000

    added another entry in users for the restaurant. Entry name 'LightResto'

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -2,3 +2,4 @@
 INSERT INTO users (name, phone, email) VALUES ('Alice', '1029384756', 'alice@test.com');
 INSERT INTO users (name, phone, email) VALUES ('Bob', '3847302927', 'bob@test.com');
 INSERT INTO users (name, phone, email) VALUES ('Nutella', '4756473820', 'nutella@test.com');
+INSERT INTO users (name, phone, email) VALUES ('LightResto', '3947583928', 'lightResto@test.com');
