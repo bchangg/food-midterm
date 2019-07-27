841f87bcf1336d0a9ea3eb6f3c5650b08c3a020e
mroger <rene.mroger@gmail.com>
2019-07-27 22:37:09 +0000

    seeds update

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,5 +1,5 @@
 -- Users table seeds here (Example)
 INSERT INTO users (name, phone, email) VALUES ('Alice', '1029384756', 'alice@test.com');
 INSERT INTO users (name, phone, email) VALUES ('Bob', '3847302927', 'bob@test.com');
-INSERT INTO users (name, phone, email) VALUES ('Nutella', '4756473820', 'nutella@test.com');
-INSERT INTO users (name, phone, email) VALUES ('LightResto', '3947583928', 'lightResto@test.com');
+INSERT INTO users (name, phone, email) VALUES ('Frodo', '4756473820', 'frodo@test.com');
+INSERT INTO users (name, phone, email) VALUES ('Restaurant', '3947583928', 'Restaurant@test.com');
--- server.js
+++ server.js
@@ -2,13 +2,16 @@
 require('dotenv').config();
 
 // Web server config
-const PORT       = process.env.PORT || 8080;
-const ENV        = process.env.ENV || "development";
-const express    = require("express");
+const PORT = process.env.PORT || 8080;
+const ENV = process.env.ENV || "development";
+const express = require("express");
 const bodyParser = require("body-parser");
-const sass       = require("node-sass-middleware");
-const app        = express();
-const morgan     = require('morgan');
+const sass = require("node-sass-middleware");
+const app = express();
+const morgan = require('morgan');
+const usersRoutes = require("./routes/users");
+const restaurantsRoutes = require("./routes/restaurants");
+
 
 // PG database client/connection setup
 const { Pool } = require('pg');
@@ -33,8 +36,6 @@ app.use(express.static("public"));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
-const usersRoutes = require("./routes/users");
-const restaurantsRoutes = require("./routes/restaurants");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
