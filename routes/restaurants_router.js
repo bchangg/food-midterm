a9137429b152e76ada9238ab5bd0cc3856581e7f
mroger <rene.mroger@gmail.com>
2019-07-28 20:37:54 +0000

    database minor update and restaurant initial ejs implementation

--- routes/restaurants_router.js
+++ routes/restaurants_router.js
@@ -1,22 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+const getPendingAndInProgressOrders = `select * from orders WHERE LOWER(order_status) = LOWER('Pending') OR LOWER(order_status) = LOWER('Accepted')`;
+
 module.exports = (db) => {
   router.get("/", (req, res) => {
     // render order status
     console.log("I HAVE REACHED RESTO page");
 
     res.render("restaurant", { user: true });
-    // db.query(`SELECT * FROM orders;`)
-    //   .then(data => {
-    //     const orders = data.rows;
-    //     res.json({ orders });
-    //   })
-    //   .catch(err => {
-    //     res
-    //       .status(500)
-    //       .json({ error: err.message });
-    //   });
+    db.query(getPendingAndInProgressOrders)
+      .then(data => {
+        console.log(data.rows);
+        const orders = data.rows;
+        res.json({ orders });
+      })
+      .catch(err => {
+        // console.log('error:', err);
+        // res.status(500)
+        //   .json({ error: err.message });
+      });
   });
   return router;
 };
