86244369c0e7869ade640e284dc9c10018193bfc
mroger <rene.mroger@gmail.com>
2019-07-28 23:07:26 +0000

    creating dynamic trable creation for each order

--- routes/restaurants_router.js
+++ routes/restaurants_router.js
@@ -1,19 +1,39 @@
 const express = require('express');
 const router = express.Router();
 
-const getPendingAndInProgressOrders = `select * from orders WHERE LOWER(order_status) = LOWER('Pending') OR LOWER(order_status) = LOWER('Accepted')`;
+const getPendingAndInProgressOrders = `
+select orders.id AS order_id, orders.order_status, users.phone, users.name from orders
+RIGHT JOIN users ON orders.user_id = users.id
+WHERE LOWER(order_status) = LOWER('Pending')
+OR LOWER(order_status) = LOWER('Ready')
+OR LOWER(order_status) = LOWER('Preparing')
+ORDER BY CASE
+  WHEN orders.order_status ='Pending' THEN 1
+  WHEN orders.order_status ='Preparing' THEN 2
+  WHEN orders.order_status ='Ready' THEN 3
+  WHEN orders.order_status ='Complete' THEN 4
+  ELSE 5
+END;`;
+
+const getItemsPerOrder = `
+SELECT dishes.name AS dish_name, orders_details.order_id  AS order_id from orders_details
+JOIN dishes ON orders_details.dish_id = dishes.id
+GROUP BY orders_details.order_id, dishes.name
+ORDER BY orders_details.order_id;`;
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
     // render order status
     console.log("I HAVE REACHED RESTO page");
 
-    res.render("restaurant", { user: true });
     db.query(getPendingAndInProgressOrders)
-      .then(data => {
-        console.log(data.rows);
-        const orders = data.rows;
-        res.json({ orders });
+      .then(ordersFromQuery => {
+        db.query(getItemsPerOrder)
+          .then(itemsFromQuery => {
+            const items = itemsFromQuery.rows;
+            const orders = ordersFromQuery.rows;
+            res.render("restaurant", { user: true, orders: orders, items: items });
+          })
       })
       .catch(err => {
         // console.log('error:', err);
