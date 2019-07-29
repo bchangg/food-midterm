b8fcfcb91f675fafd1d3bd2f7b9bfdddd7397d98
ssspiderzzz <ssspiderzzz@gmail.com>
2019-07-29 18:43:51 +0000

    update the seeds

--- db/seeds/03_orders.sql
+++ db/seeds/03_orders.sql
@@ -3,14 +3,10 @@ VALUES ('Pending', 1);
 INSERT INTO orders (order_status, user_id)
 VALUES ('Pending', 2);
 INSERT INTO orders (order_status, user_id)
-<<<<<<< HEAD
-VALUES ('Accepted', 2);
-=======
 VALUES ('Preparing', 2);
->>>>>>> master
 INSERT INTO orders (order_status, user_id)
 VALUES ('Pending', 3);
 INSERT INTO orders (order_status, user_id)
 VALUES ('Ready', 3);
 INSERT INTO orders (order_status, user_id)
-VALUES ('Completed', 3);
+VALUES ('Complete', 3);
