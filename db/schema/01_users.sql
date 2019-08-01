423bc323deea2cfaa93912d0ac9e8218e91ec817
mroger <rene.mroger@gmail.com>
2019-08-01 00:25:17 +0000

    fixed schema

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,8 +1,7 @@
 DROP TABLE IF EXISTS users CASCADE;
-CREATE SEQUENCE IF NOT EXISTS users_id_seq;
 
 CREATE TABLE users (
-  id  INT UNIQUE NOT NULL DEFAULT NEXTVAL('users_id_seq'),
+  id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   phone char(10) not null,
   email varchar(255)
